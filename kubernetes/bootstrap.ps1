b34d5cc3bbed49abed099716b34fbdc37420245b
Jochem De Jaeghere <jochemdejaeghere@hotmail.com>
2020-08-31 21:29:04 +0200

    Add ingress for argocd

--- kubernetes/bootstrap.ps1
+++ kubernetes/bootstrap.ps1
@@ -1,3 +1,4 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch deploy argocd-server -n argocd -p '[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--insecure"}]' --type json
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.0-beta.1/cert-manager.crds.yaml
